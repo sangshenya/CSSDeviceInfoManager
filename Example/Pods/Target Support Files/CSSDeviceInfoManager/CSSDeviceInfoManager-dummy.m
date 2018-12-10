8f5dd5bd36419e8270f7cbcd2164591b853c8acb
陈坤 <sangshen@ecook.cn>
2018-12-10 15:50:10 +0800

    first commit

---
+++ Example/Pods/Target Support Files/CSSDeviceInfoManager/CSSDeviceInfoManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CSSDeviceInfoManager : NSObject
+@end
+@implementation PodsDummy_CSSDeviceInfoManager
+@end
---
+++ Example/Pods/Target Support Files/Pods-CSSDeviceInfoManager_Tests/Pods-CSSDeviceInfoManager_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CSSDeviceInfoManager_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CSSDeviceInfoManager_Tests
+@end
